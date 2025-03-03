60bef3c3ffd873d7d1d2f52df4e8b01441b41a29
Eryk <erykhalicki0@gmail.com>
2025-03-02 22:14:49 -0800

    added multiplie servo control

--- src/microcontroller/servo_control_test/servo_control_test.ino
+++ src/microcontroller/servo_control_test/servo_control_test.ino
@@ -1,20 +1,32 @@
 #include <ESP32Servo.h>
 
 // create four servo objects 
-Servo servo1;
+Servo servo1, servo2, servo3, servo4;
 
-// Published values for SG90 servos; adjust if needed
-int minUs = 500;
-int maxUs = 2500;
+int servo1Pin = 4;
+// Define throttle signal values
+const int minThrottle = 500; // Minimum throttle in microseconds (1ms)
+const int maxThrottle = 2500; // Maximum throttle in microseconds (2ms)
 
-int servo1Pin = 2;
-
-int pos = 90;      // position in degrees
+int pins[] = {4, 16, 17, 5};
+float pos[] = {90,90,90,90};      // position in degrees
+float goal[] = {90,90,90,90};
+float speed = 0.5;
+Servo motors[8];
 
 void setup() {
-  Serial.begin(9600);
-  servo1.setPeriodHertz(50);      // Standard 50hz servo
-  servo1.attach(servo1Pin, minUs, maxUs);
+  // Initialize PWM for the ESC
+  for (int i = 0; i < 4; i++) {
+    motors[i] = Servo();
+    motors[i].setPeriodHertz(50);
+    motors[i].attach(pins[i]);
+  }
+
+  Serial.begin(115200);
+  while(Serial.available() > 0){
+    Serial.read();
+  }
+  
 }
 
 int sign(int i){
@@ -22,17 +34,57 @@ int sign(int i){
   return 1;
 }
 
-void moveTo(int goal){
-  while(pos != goal){
-    servo1.write(pos);
-    pos += sign(goal - pos);
-    delay(10);  
-  }
+const byte numChars = 64;
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+    
+    if (Serial.available() > 0) {
+        rc = Serial.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+int currMotor = 0;
+void parseNewData(){
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strtokIndx = strtok(receivedChars,"<");      // get the first part - the string
+    currMotor=atoi(strtokIndx);
+
+    strtokIndx = strtok(NULL, "<");
+    goal[currMotor] = atof(strtokIndx);     // convert this part to a float
+    newData = false;
 }
 
 void loop() {
-  if(Serial.available() > 1){
-    moveTo(Serial.parseInt());
+  // Check for serial input
+  recvWithEndMarker();
+  if(newData)parseNewData();
+
+  for (int i = 0; i < 4; i++) {
+    motors[i].write(pos[i]);
+    if(pos[i] + speed > goal[i] || pos[i] - speed < goal[i]){
+      pos[i] += sign(goal[i] - pos[i]) * speed;  
+    }
+    else{pos[i] = goal[i];}
   }
-  servo1.write(pos);
+  
+  delay(10);
 }
