5a394c3d77c288a26016edcee184ec8ceb91d9c7
Eryk <erykhalicki0@gmail.com>
2025-04-30 23:01:40 -0700

    Update motor control method names and pin configurations
    - Replace drive -> motorGo
    - Replace brake -> motorBrake
    - Update DC motor pin configurations

---
+++ src/microcontroller/zima_hardware_interface/config.h
@@ -0,0 +1,28 @@
+#ifndef ZIMA_HARDWARE_CONFIG_H
+#define ZIMA_HARDWARE_CONFIG_H
+
+// Servo Motor Configuration
+#define SERVO_COUNT 8
+const int SERVO_PINS[] = {16, 27, 25, 14, 26, 18, 17, 19};
+
+// Rotary Encoder Configuration
+#define ENCODER_PIN_LEFT 34   // Left encoder pin
+#define ENCODER_PIN_RIGHT 35  // Right encoder pin
+
+// DC Motor Configuration
+#define DCMOTOR_LEFT_PINA 15
+#define DCMOTOR_LEFT_PINB 2
+#define DCMOTOR_RIGHT_PINA 22
+#define DCMOTOR_RIGHT_PINB 23
+
+#define DCMOTOR_RES 8
+#define DCMOTOR_FREQ 5000
+
+// Serial Communication
+#define SERIAL_BAUD 115200
+
+// Timing and Debounce
+#define ENCODER_DEBOUNCE_DELAY 5  // ms
+#define MAIN_LOOP_DELAY 3        // ms
+
+#endif // ZIMA_HARDWARE_CONFIG_H
--- src/microcontroller/zima_hardware_interface/dcmotor_control.h
+++ src/microcontroller/zima_hardware_interface/dcmotor_control.h
@@ -6,38 +6,57 @@
 
 class DCMotorController {
 private:
-    FS_MX1508 motorA;
+    MX1508 motorLeft;
+    MX1508 motorRight;
 
 public:
     DCMotorController() : 
-        motorA(DCMOTOR_PINA, DCMOTOR_PINB) 
+        motorLeft(DCMOTOR_LEFT_PINA, DCMOTOR_LEFT_PINB),
+        motorRight(DCMOTOR_RIGHT_PINA, DCMOTOR_RIGHT_PINB)
     {}
 
     void init() {
-        // Optional: Set PWM frequency and resolution if needed
-        motorA.setPWMFrequency(DCMOTOR_FREQ);
-        motorA.setPWMResolution(DCMOTOR_RES);
+        // Set PWM frequency and resolution for both motors
+        motorLeft.setFrequency(DCMOTOR_FREQ);
+        motorLeft.setResolution(DCMOTOR_RES);
+        
+        motorRight.setFrequency(DCMOTOR_FREQ);
+        motorRight.setResolution(DCMOTOR_RES);
     }
 
-    void forward(int speed) {
-        // Ensure speed is within 0-255 range
+    void leftMotorForward(int speed) {
         speed = constrain(speed, 0, 255);
-        motorA.drive(speed);
+        motorLeft.motorGo(speed);
     }
 
-    void reverse(int speed) {
-        // Ensure speed is within 0-255 range
+    void leftMotorReverse(int speed) {
         speed = constrain(speed, 0, 255);
-        motorA.drive(-speed);
+        motorLeft.motorGo(-speed);
     }
 
-    void stop(bool softStop = true) {
+    void rightMotorForward(int speed) {
+        speed = constrain(speed, 0, 255);
+        motorRight.motorGo(speed);
+    }
+
+    void rightMotorReverse(int speed) {
+        speed = constrain(speed, 0, 255);
+        motorRight.motorGo(-speed);
+    }
+
+    void leftMotorStop(bool softStop = true) {
         if (softStop) {
-            // Gradual stop by reducing speed to 0
-            motorA.drive(0);
+            motorLeft.motorGo(0);
         } else {
-            // Immediate hard stop
-            motorA.brake();
+            motorLeft.motorBrake();
+        }
+    }
+
+    void rightMotorStop(bool softStop = true) {
+        if (softStop) {
+            motorRight.motorGo(0);
+        } else {
+            motorRight.motorBrake();
         }
     }
 };
