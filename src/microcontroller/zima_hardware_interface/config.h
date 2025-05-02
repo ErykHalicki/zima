e1bd5f4a897353745fa15e4f6d4526107e58175c
Eryk <erykhalicki0@gmail.com>
2025-05-02 13:31:20 -0700

    minor config changes

--- src/microcontroller/zima_hardware_interface/config.h
+++ src/microcontroller/zima_hardware_interface/config.h
@@ -6,14 +6,14 @@
 const int SERVO_PINS[] = {16, 27, 25, 14, 26, 18, 17, 19};
 
 // Rotary Encoder Configuration
-#define ENCODER_PIN_LEFT 34   // Left encoder pin
-#define ENCODER_PIN_RIGHT 35  // Right encoder pin
+#define ENCODER_PIN_LEFT 35   // Left encoder pin
+#define ENCODER_PIN_RIGHT 34  // Right encoder pin
 
 // DC Motor Configuration
-#define DCMOTOR_LEFT_PINA 15
-#define DCMOTOR_LEFT_PINB 2
-#define DCMOTOR_RIGHT_PINA 22
-#define DCMOTOR_RIGHT_PINB 23
+#define DCMOTOR_LEFT_PINA 22
+#define DCMOTOR_LEFT_PINB 23
+#define DCMOTOR_RIGHT_PINA 2
+#define DCMOTOR_RIGHT_PINB 15
 
 #define DCMOTOR_RES 8
 #define DCMOTOR_FREQ 5000
@@ -26,6 +26,6 @@ const int SERVO_PINS[] = {16, 27, 25, 14, 26, 18, 17, 19};
 #define MAIN_LOOP_DELAY 10        // ms
 
 // Servo Motor Movement Speed
-#define SERVO_SPEED 0.25         // Range: 0.1 - 1.0, controls servo movement speed
+#define SERVO_SPEED 0.6         // Range: 0.1 - 1.0, controls servo movement speed
 
 #endif // ZIMA_HARDWARE_CONFIG_H
