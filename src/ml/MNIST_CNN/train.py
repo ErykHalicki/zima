daabc7a99525a02a274c7976aac3807a140d38c4
Eryk Halicki <erykhalicki0@gmail.com>
2025-10-20 22:07:13 -0700

    98% MNIST

--- src/ml/MNIST_CNN/train.py
+++ src/ml/MNIST_CNN/train.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
 
 # roughly following https://docs.pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 
@@ -47,7 +48,7 @@ class SmallConvNet(nn.Module):
     def __init__(self, 
                  input_image_size=28, 
                  convolution_kernel_width=5, 
-                 convolution_kernel_output_dim=10,
+                 convolution_kernel_output_dim=6,
                  max_pool_width=2):
         super().__init__()
         self.image_size = input_image_size
@@ -58,23 +59,26 @@ class SmallConvNet(nn.Module):
         convolution_layer_output_size = (input_image_size - convolution_kernel_width) + 1 
         self.max_pool = nn.MaxPool2d(max_pool_width, max_pool_width) #2x2 maxpool, for dim reduction (and nonlinearity?)
         
-        self.linear_layer = nn.Linear(int(convolution_layer_output_size / max_pool_width)**2 * convolution_kernel_output_dim, 100) #classifier
-        self.classifier = nn.Linear(100, 10)
+        self.linear_layer_1 = nn.Linear(int(convolution_layer_output_size / max_pool_width)**2 * convolution_kernel_output_dim, 200) #classifier
+        self.linear_layer_2 = nn.Linear(200, 50) #classifier
+        self.classifier = nn.Linear(50, 10)
 
     def forward(self, batch):
         if batch.size()[2] != self.image_size or batch.size()[2] != self.image_size:
             raise Exception("Size of passed tensor does not match intended image size!")
         result = self.max_pool(F.relu(self.convolution_layer(batch)))
         result = torch.flatten(result, start_dim=1)
-        result = F.relu(self.linear_layer(result))
+        result = F.relu(self.linear_layer_1(result))
+        result = F.relu(self.linear_layer_2(result))
         result = self.classifier(result)
         return result
 
 small_conv_net = SmallConvNet().to(main_device)
 cross_entropy_loss = nn.CrossEntropyLoss()
 optimizer = optim.SGD(small_conv_net.parameters(), lr=0.001, momentum=0.9)
+scheduler = StepLR(optimizer, step_size=10, gamma=0.5)
 
-for epoch in range(10):
+for epoch in range(30):
     running_loss = 0.0
     for train_batch_index, train_batch_tuple in enumerate(mnist_train_dataloader):
         images, labels = train_batch_tuple
@@ -94,6 +98,7 @@ for epoch in range(10):
         if train_batch_index % 100 == 99:    # print every 100 mini-batches
             print(f'[epoch: {epoch + 1:3d}, batch: {train_batch_index + 1:5d}] loss: {running_loss / 100:.3f}')
             running_loss = 0.0
+    scheduler.step()
 
     total_correct_predictions = total_predictions = 0    
     with torch.no_grad(): #dont calculate gradients to speed up inference
