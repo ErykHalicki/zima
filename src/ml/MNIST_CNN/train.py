95e6cb4f83bcb81cd51d4d91ea2c254cb5e5f5de
Eryk Halicki <erykhalicki0@gmail.com>
2025-10-20 21:23:16 -0700

    LETS GOOOOOOOO

--- src/ml/MNIST_CNN/train.py
+++ src/ml/MNIST_CNN/train.py
@@ -5,16 +5,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 # roughly following https://docs.pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 
-MNIST_PATH = '/Users/erykhalicki/Documents/projects/current/zima/src/ml/MNIST_CNN/'
+MNIST_PATH = '/home/eryk/Documents/projects/zima/src/ml/MNIST_CNN'
 #TRAINING PARAMETERS
 batch_size = 16
 
 main_device = torch.device("cpu")
 if torch.backends.mps.is_available():
     main_device = torch.device("mps")
+if torch.cuda.is_available():
+    main_device = torch.device("cuda")
 
 # transform function turns PIL image into a tensor
 PIL_to_Tensor = transforms.ToTensor()
@@ -40,18 +43,75 @@ def imshow(img):
 print(labels)
 imshow(torchvision.utils.make_grid(images))
 
-class SimpleConvNet(nn.Module):
+class SmallConvNet(nn.Module):
     def __init__(self, 
                  input_image_size=28, 
                  convolution_kernel_width=3, 
-                 convolution_kernel_output_dim=3):
-        super.__init__()
-        self.input_size = input_image_size
+                 convolution_kernel_output_dim=3,
+                 max_pool_width=2):
+        super().__init__()
+        self.image_size = input_image_size
         self.convolution_layer = nn.Conv2d(1, 
                                            convolution_kernel_output_dim, 
                                            convolution_kernel_width) # 1 input (grayscale),n output layers, nxn kernel
-        convolution_layer_output_size = (self.input_size - ) + 1 
-        #calculate output size, of pool and conv, then plug into linear input
-        self.max_pool_1 = nn.MaxPool2d(2,2) #2x2 maxpool
+
+        convolution_layer_output_size = (input_image_size - convolution_kernel_width) + 1 
+        self.max_pool = nn.MaxPool2d(max_pool_width, max_pool_width) #2x2 maxpool, for dim reduction (and nonlinearity?)
         
-        self.linear_layer_1 = nn.Linear(100, 10) #classifier
+        self.linear_layer = nn.Linear(int(convolution_layer_output_size / max_pool_width)**2 * convolution_kernel_output_dim, 100) #classifier
+        self.classifier = nn.Linear(100, 10)
+
+    def forward(self, batch):
+        if batch.size()[2] != self.image_size or batch.size()[2] != self.image_size:
+            raise Exception("Size of passed tensor does not match intended image size!")
+        result = self.max_pool(F.relu(self.convolution_layer(batch)))
+        result = torch.flatten(result, start_dim=1)
+        result = F.relu(self.linear_layer(result))
+        result = self.classifier(result)
+        return result
+
+small_conv_net = SmallConvNet().to(main_device)
+cross_entropy_loss = nn.CrossEntropyLoss()
+optimizer = optim.SGD(small_conv_net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(3):
+    running_loss = 0.0
+    for i, batch_tuple in enumerate(mnist_train_dataloader):
+        images, labels = batch_tuple
+        images = images.to(main_device)
+        labels = labels.to(main_device)
+        optimizer.zero_grad()
+
+        logits = small_conv_net(images)
+        loss = cross_entropy_loss(logits, labels)
+        # probability calculated via softmax = exp(correct class prediction) / sum(exp(all class predictions))
+        # -ln(p_correct_class) is cross entropy loss
+        # -ln(1) = 0 (correct) -> -ln(0) approaches inf
+        loss.backward() # calculate gradient of each layer w.r.t loss
+        optimizer.step() # do W = W - grad*lr
+        running_loss += loss.item() #extract numeric value of the loss
+
+        if i % 100 == 99:    # print every 100 mini-batches
+            print(f'[epoch: {epoch + 1:3d}, batch: {i + 1:5d}] loss: {running_loss / 100:.3f}')
+            running_loss = 0.0
+
+MODEL_SAVE_PATH = './simple_mnist_conv_net.pt'
+torch.save(small_conv_net.state_dict(), MODEL_SAVE_PATH)
+
+mnist_test_data_iterator = iter(mnist_test_dataloader)
+images, labels = next(mnist_test_data_iterator)
+
+
+small_conv_net = SmallConvNet()
+small_conv_net.load_state_dict(torch.load(MODEL_SAVE_PATH, weights_only=True))
+# re load the model for practice
+
+logits = small_conv_net(images)
+max_logits, predictions = torch.max(logits, dim=1) 
+# getting max along dim 1, since dim 0 would return the sample that had highest confidence in the batch (for each class), not the highest confidence class in each sample
+print(f'Ground Truth: {labels}')
+print(f"Predictions: {predictions}")
+imshow(torchvision.utils.make_grid(images))
+
+
+
