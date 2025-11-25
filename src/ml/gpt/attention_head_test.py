572cec3a51f4bc8f7ec601b238a1c259df3ba0b6
Eryk Halicki <erykhalicki0@gmail.com>
2025-11-24 23:07:08 -0800

    attention head

---
+++ src/ml/gpt/attention_head_test.py
@@ -0,0 +1,12 @@
+from models.transformer import AttentionHead
+import torch
+d_model = 100
+d_keys = d_model
+seq_len = 1000
+batch_size = 32
+x = torch.randn(batch_size,seq_len,d_model)
+
+a_head = AttentionHead(d_model, d_keys)
+mask = torch.ones(batch_size, seq_len, seq_len).tril()
+
+print(a_head(x, mask))
---
+++ src/ml/gpt/models/transformer.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import numpy as np 
+
+class AttentionHead(nn.Module):
+    def __init__(self,
+                 d_model,
+                 d_keys):
+        super().__init__()
+        self.d_model = d_model
+        self.d_keys = d_keys
+        self.root_d_keys = d_keys**(1/2)
+        self.query_weights = nn.Linear(d_model, d_keys)
+        self.key_weights = nn.Linear(d_model, d_keys)
+        self.value_weights = nn.Linear(d_model, d_keys)
+        self.softmax = nn.Softmax(dim=2) # softmax over the columns (s.t. every row sums to 1)
+
+    def forward(self, x, mask=None):
+        '''
+        x: batch of stacked sequence row vectors (batch, d_seq, d_model)
+        mask (Optional): if provided, will mask the attention matrix (batch, d_seq, d_seq) 1 to keep, 0 to mask
+        '''
+        Q = self.query_weights(x)
+        K = self.key_weights(x)
+        V = self.value_weights(x)
+        return self.scaled_dot_product_attention(Q, K, V, mask)
+
+    def scaled_dot_product_attention(self, Q, K, V, mask=None):
+        '''
+        Q: Query matrix (each row represents "what is this token looking for"), (batch x d_seq x d_keys)
+        K: Key matrix (each row represents "what does this token have"), (batch x d_seq x d_keys)
+        V: Value matrix (each row represents "what is the information in this token?"), (batch x d_seq x d_keys)
+        '''
+        K_t = K.transpose(1,2) # (batch, d_seq, d_keys) -> (batch, d_keys, d_seq)
+        scaled_attention_matrix = Q@K_t/self.root_d_keys # (d_seq, d_keys) x (d_keys, d_seq) -> (d_seq, d_seq)
+        if not mask is None:
+            scaled_attention_matrix.masked_fill_(mask == 0, float('-inf'))
+        # attention matrix[i,j] says, "how much does token i care about token j"
+        normalized_attention_matrix = self.softmax(scaled_attention_matrix) 
+        # every row now represents a weight vector for each token in the sequence, between 0-1, summing to 1.  
+        return normalized_attention_matrix @ V # (d_seq, d_seq) x (d_seq, d_keys) -> (d_seq, d_keys)
+        # every row still corresponds to one token, but now the token has been enriched with meaning from the other tokens in the sequence that matter to it    
+
