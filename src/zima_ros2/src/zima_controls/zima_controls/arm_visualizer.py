f63dd97622a6e5b045281f3369aac8d647a792a1
ErykHalicki <erykhalicki0@gmail.com>
2025-12-07 15:43:35 -0800

    Forward Kinematics done

---
+++ src/zima_ros2/src/zima_controls/zima_controls/arm_visualizer.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.widgets import Slider
+
+
+def visualize_arm(points):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    points = np.array(points)
+    xs = points[:, 0]
+    ys = points[:, 1]
+    zs = points[:, 2]
+
+    ax.plot(xs, ys, zs, 'o-', linewidth=2, markersize=8)
+    ax.scatter(xs[0], ys[0], zs[0], c='green', s=100, label='Base')
+    ax.scatter(xs[-1], ys[-1], zs[-1], c='red', s=100, label='End Effector')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.legend()
+    ax.set_title('Robot Arm Visualization')
+
+    plt.show()
+
+
+def visualize_interactive(solver, initial_joints=None):
+    if initial_joints is None:
+        initial_joints = [0.0] * len(solver.revolute_links)
+
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    plt.subplots_adjust(bottom=0.25)
+
+    def update_arm(joints):
+        ax.clear()
+        transforms = solver.forward(joints)
+        points = np.array([t[:3, 3] for t in transforms])
+
+        xs = points[:, 0]
+        ys = points[:, 1]
+        zs = points[:, 2]
+
+        ax.plot(xs, ys, zs, 'o-', linewidth=2, markersize=8)
+        ax.scatter(xs[0], ys[0], zs[0], c='green', s=100, label='Base')
+        ax.scatter(xs[-1], ys[-1], zs[-1], c='red', s=100, label='End Effector')
+
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        ax.legend()
+        ax.set_title(f'End Effector: ({xs[-1]:.3f}, {ys[-1]:.3f}, {zs[-1]:.3f})')
+
+        max_range = 0.2
+        ax.set_xlim([-max_range, max_range])
+        ax.set_ylim([-max_range, max_range])
+        ax.set_zlim([-max_range, max_range])
+
+        fig.canvas.draw_idle()
+
+    sliders = []
+    for i in range(len(solver.revolute_links)):
+        ax_slider = plt.axes([0.1, 0.15 - i*0.03, 0.8, 0.02])
+        slider = Slider(ax_slider, f'Joint {i+1}', -np.pi, np.pi, valinit=initial_joints[i])
+        sliders.append(slider)
+
+    def update(val):
+        joints = [s.val for s in sliders]
+        update_arm(joints)
+
+    for slider in sliders:
+        slider.on_changed(update)
+
+    update_arm(initial_joints)
+    plt.show()
--- src/zima_ros2/src/zima_controls/zima_controls/ik_solver.py
+++ src/zima_ros2/src/zima_controls/zima_controls/ik_solver.py
@@ -1,19 +1,51 @@
 import numpy as np
 import yaml
-
-# need knowledge of arm structure (yaml file isntead of urdf)
-# load arm structure from yaml on __init__(yaml_path)
-# generate transformation matrices on init
-# calculate pseudoinverse on init
-# on solve() call take in xyzrpy in meters and radians
-# use pre calculated pinv to get joint state output
-# add biases to joint states
-# return joint state in radians
+from scipy.spatial.transform import Rotation
 
 class IKSolver:
     def __init__(self, arm_structure_yaml_path):
         with open(arm_structure_yaml_path, 'r') as f:
             structure_data = yaml.safe_load(f)
-        arm_links = structure_data['links']
-        for link in arm_links:
-            print(link)
+        self.links = structure_data['links']
+        self.revolute_links = [link for link in self.links if link['type']=='revolute']
+        
+    def forward(self,joints):
+        if len(joints) != len(self.revolute_links):
+            raise Exception("Cannot do FK with more joint angles than revolute links")
+
+        T = [np.eye(4)]
+        i=0
+        for link in self.links:
+            axis = np.array(link['axis'])/np.linalg.norm(link['axis'])
+            if link['type'] == 'revolute':
+                R = Rotation.from_rotvec((joints[i] + link['bias']) * axis)
+                i+=1
+            elif link['type'] == 'fixed':
+                R = Rotation.from_rotvec(link['bias'] * axis)
+            T_link = np.eye(4)
+            T_link[:3,:3] = R.as_matrix()
+            T_link[:3,3] = link['translation']
+            T.append(T[-1]@T_link)
+        return T
+
+    def solve(self,xyz,rpy):
+        pass
+    
+
+    def matrix_to_euler(matrix):
+        R = matrix[0:3, 0:3]
+        r = Rotation.from_matrix(R)
+        return r.as_euler('xyz', degrees=False)
+
+# on solve() call take in xyzrpy in meters and radians
+# use pre calculated pinv to get joint state output
+# add biases to joint states
+# return joint state in radians
+
+if __name__ == '__main__':
+    from arm_visualizer import visualize_interactive
+
+    yaml_path = "/Users/erykhalicki/Documents/projects/current/zima/src/zima_ros2/src/zima_controls/arm_data/4dof_arm_structure.yaml"
+    solver = IKSolver(yaml_path)
+    visualize_interactive(solver)
+    
