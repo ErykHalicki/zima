994d167ca40f5cc2adbd9faafc6b6d85b04c8e26
ErykHalicki <erykhalicki0@gmail.com>
2026-02-22 18:45:57 +0100

    conitnued reorg

--- scripts/zima/cloud_setup.bash
+++ scripts/zima/cloud_setup.bash
@@ -2,17 +2,13 @@ if [ ! -d "zima" ]; then
     echo "Cloning zima repository..."
     git clone https://github.com/ErykHalicki/zima.git
     cd zima/src
+    pip install -r requirements.txt
+    hf auth login --token $HF_TOKEN
 else
     echo "Zima repository already exists, skipping clone"
     cd zima/src
     git pull
 fi
 
-
-pip install -r requirements.txt
-
-hf auth login --token $HF_TOKEN
-
 source scripts/train_act.bash
-
 
